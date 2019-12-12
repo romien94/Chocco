80cbf63c79326fbcec3e9052c555a4eea22b4103
Roman <48430050+romien94@users.noreply.github.com>
2019-12-12 14:21:26 +0300

    bars js updated

--- js/bars.js
+++ js/bars.js
@@ -1,20 +1,21 @@
-let barsItem = document.querySelectorAll('.bars-slider__item');
-let right = document.querySelector('.bars__button--right');
-let left = document.querySelector('.bars__button--left');
+let barsItems = document.querySelectorAll('.bars-slider__item');
+let buttonLeft = document.querySelector('.bars__button--left');
+let buttonRight = document.querySelector('.bars__button--right');
 
-for (let i = 0; i < barsItem.length; i++) {
-  let barsActive = document.querySelector('.bars-slider__item--active');
-  right.addEventListener('click', function(e) {
-    e.preventDefault();
-    i++;
-    barsActive.classList.remove('bars-slider__item--active');
-    barsItem[i].classList.add('bars-slider__item--active');
-  });
+buttonLeft.addEventListener('click',function(e) {
+ loop('left', e); 
+});
 
-  left.addEventListener('click', function(e) {
-    e.preventDefault();
-    i--;
-    barsActive.classList.remove('bars-slider__item--active');
-    barsItem[i].classList.add('bars-slider__item--active');
-  })
-}
+buttonRight.addEventListener('click',function(e) {
+  loop('right', e);
+});
+
+function loop(direction, e) {
+  e.preventDefault;
+  if (direction === 'right') {
+    barsItems.appendChild(barsItems.firstElementChild);
+  }
+  if (direction === 'left') {
+    barsItems.insertBefore(barsItems.lastElementChild,barsItems.firstElementChild);    
+  }
+};
