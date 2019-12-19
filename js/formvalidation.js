308262741811a5dd4f3f14a46c4d9d220196c1ac
Roman Uskov <romanuskov94@gmail.com>
2019-12-19 21:46:29 +0300

    modal window added for form, form validation script rewrited, basic animation to the menu accordeon added

--- js/formvalidation.js
+++ js/formvalidation.js
@@ -3,6 +3,9 @@ const sendButton = form.querySelector('.button');
 const formBlock = document.querySelectorAll('.form__block');
 const formElements = form.elements;
 const input = document.querySelectorAll('input');
+const modal = document.querySelector('.modal');
+const modalMessage = modal.querySelector('.modal__message');
+const modalButton = modal.querySelector('.modal__button');
 
 for (let i = 0; i < formBlock.length; i++) {
   const error = document.createElement('span');
@@ -25,38 +28,34 @@ sendButton.addEventListener('click',(e) => {
   const xhr = new XMLHttpRequest();
   xhr.open('POST','./data.json');
   xhr.send(JSON.stringify(data));
+   modalMessage.textContent = 'Сообщение было успешно отправлено';
+   modal.classList.add('modal--active');
+ } else {
+   modalMessage.textContent = 'Произошла ошибка';
+   modal.classList.add('modal--active');
  }
+ modalButton.addEventListener('click', (evt) => {
+   evt.preventDefault();
+   modal.classList.remove('modal--active');
+ })
 })
 
 function validateForm(form) {
+  let valid = true;
   for (let i = 0; i < formElements.length; i++) {
     validateField(formElements[i]);
-  }
+    if (!validateField(formElements[i])) valid = false;
+    }
+  return valid;
 }
 
 function validateField(field) {
   if (!field.checkValidity()) {
     field.parentNode.lastChild.textContent = field.validationMessage;
+    return false;
   }
   else {
     field.parentNode.lastChild.textContent = '';
+    return true;
   }
 }
-
-/*
-
-  const data = {
-    name: form.elements.name.value,
-    lastName: form.elements.lastName.value,
-    email: form.elements.email.value
-  };
-  console.log(data);
-  if (validateForm(form)) {
-    const xhr = new XMLHttpRequest();
-    xhr.open('POST','');
-    xhr.send(JSON.stringify(data));
-  };
-})
-
-
-*/
